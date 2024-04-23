4edcd5ca841b7e6bb7c8222ad7d548a33567b0fc
Sophia-Guiter <sophia.guiter@marquette.edu>
2024-04-23 15:08:56 -0500

    Create CustomerAppointment Class

---
+++ CustomerAppointment.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace ClassProject001;
+
+public class CustomerAppointment
+{
+    public Customer c {get;set;}
+    public Appointment a {get; set;}
+
+    public CustomerAppointment(Customer c,Appointment a) //combination of 2 becomes a primary key 
+    {
+        this.c = c;
+        this.a = a;
+    }
+
+}
