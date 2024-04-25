d6c779fb2f708c5edc51422e849f8bc69b3c9b08
Ben <benjamin.alban@marquette.edu>
2024-04-25 15:07:44 -0500

    Added Customers class

---
+++ Customers.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassProject001;
+
+public class Customers
+{
+    public List<Customer> customerList {get; set;}
+
+    public Customers()
+    {
+        customerList = new List<Customer>();
+    }
+
+    public Customer Authenticate(string username, string password)
+    {
+        var c = customerList.Where(o => (o.Username == username) && (object.Password == password) );
+
+        if(c.Count() > 0)
+        {
+            return c.First();
+        }
+        else
+        {
+            return null; 
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        System.Console.WriteLine("What are you getting on the DB test");
+        System.Console.WriteLine("HUNDRED! 100!"); // 
     }
 }
